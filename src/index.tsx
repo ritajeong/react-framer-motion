f5ff1af0ae3142d6ecfb762e866609503b1b3ace
ritajeong <ritajeong.dev@gmail.com>
2022-08-29 15:36:28 +0900

    Configure project setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import { render } from "react-dom";
+import { motion } from "framer-motion";
+import "./index.css";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <div className="example-container">
+      <motion.div animate={{ y: 100 }}></motion.div>
+    </div>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(<App />, document.getElementById("root"));
